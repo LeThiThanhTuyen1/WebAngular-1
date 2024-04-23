ecc5b1fe488be5ebb9993537c7a06c8b271b2257
LeThiThanhTuyen1 <thanhtuyen162003@gmail.com>
2024-04-23 15:41:18 +0700

    update

--- stock-market/src/app/stock/stock-list/stock-list.component.ts
+++ stock-market/src/app/stock/stock-list/stock-list.component.ts
@@ -4,6 +4,7 @@ import { Stock } from '../../model/stock';
 import { StockItemComponent } from '../stock-item/stock-item.component';
 import { Observable, of, throwError } from 'rxjs';
 import { HttpServerService } from '../../services/http-server.service';
+import { response } from 'express';
 
 @Component({
   selector: 'app-stock-list',
@@ -17,6 +18,25 @@ export class StockListComponent implements OnInit{
   constructor(private stockService: StockService) {}
 
   ngOnInit(): void {
-      this.stocks$ = this.stockService.getStocks();
+    this.stocks$ = this.stockService.getStocks();
+    this.stockService.getStocksAsResponse()
+      .subscribe((response) => {
+          console.log('OBSERVE "response" RESPONSE is', response);
+      });
+    
+    this.stockService.getStocksEvents()
+      .subscribe((response) => {
+          console.log('OBSERVE "events" RESPONSE is', response);
+      }); 
+      
+    this.stockService.getStocksAsString()
+      .subscribe((response) => {
+        console.log('Response Type "text" RESPONSE is', response);
+      });
+    
+    this.stockService.getStocksAsBlob()
+      .subscribe((response) => {
+          console.log('Response Type "blob" RESPONSE is', response);
+      });
   }
 }
